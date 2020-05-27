a3567beed295a52d74a7971881f62698c94bb65c
Jesus Macedo <jesus.macedo@globant.com>
2020-05-27 10:34:38 -0500

    Section 3, lesson 30. Understanding JSX.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
-
+// * this is how css is imported
 import './App.css';
 
 class App extends Component {
+    /**
+     * Always required in components and it always has to return HTML content.
+     */
     render() {
         return (
             <div className="App">
                 <h1>Hiii, duuude</h1>
             </div>
         );
+
+        // * the following code does the same as the previous return statement
+        /* return React.createElement(
+            'div',
+            { className: 'App' },
+            React.createElement('h1', null, 'Hiii, duuude')
+        ); */
     }
 }
 
