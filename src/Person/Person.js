9b6437312a1e54e1a8a7cad35d162b4feb27dfc6
Jesus Macedo <jesus.macedo@globant.com>
2020-05-27 18:51:29 -0500

    Section 3, lesson 47. Adding Two Way Binding.

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -7,12 +7,14 @@ const Person = (props) => {
     // * props.children gives us access to the nested content recieved in the component implementation.
     // * It can include text, complex structure or react components
     // > props.onClick fires the eventListener in the parent component and passes it data (if defined there)
+    // > two WAY data binding comes when the recieved value is displayed AND modified in the same element (see input)
     return (
         <div>
             <p onClick={props.onClick}>
                 I'm {props.name} and I am {props.age} years old!
             </p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.onChange} value={props.name} />
         </div>
     );
 };
